af7ba77f29a2328a72e13867741d510c619a11ae
benAnn-1 <bhann@wustl.edu>
2021-06-11 16:47:46 -0500

    Initial Commit

---
+++ covidApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  covidApp
+//
+//  Created by Ben Ann on 6/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
